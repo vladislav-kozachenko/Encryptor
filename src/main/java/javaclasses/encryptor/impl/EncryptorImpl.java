b861e837e8178f62ff76104fbcedfd489be2c983
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-08 14:31:29 +0200

    Implemented encryptor.

--- src/main/java/javaclasses/encryptor/impl/EncryptorImpl.java
+++ src/main/java/javaclasses/encryptor/impl/EncryptorImpl.java
@@ -3,6 +3,9 @@ package javaclasses.encryptor.impl;
 import com.google.common.base.CharMatcher;
 import javaclasses.encryptor.Encryptor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 
@@ -13,9 +16,38 @@ public class EncryptorImpl implements Encryptor {
 
         checkArgument(!isNullOrEmpty(message));
         final StringBuilder text = new StringBuilder(prepareInput(message));
+        final int itemNumber = (int)Math.sqrt(text.length()) + 1;
+        List<String> grid = createGrid(text, itemNumber);
 
+        return getEncryptedMessage(grid);
+    }
 
-        return null;
+    private String getEncryptedMessage(List<String> grid) {
+        final StringBuilder encryptedMessage = new StringBuilder();
+        final int wordSize = grid.get(0).length();
+        for (int item = 0; item < wordSize; item++) {
+            for (String word : grid) {
+                if (item < word.length()) {
+                    encryptedMessage.append(word.charAt(item));
+                }
+            }
+            encryptedMessage.append(' ');
+        }
+        return encryptedMessage.toString().trim();
+    }
+
+    private List<String> createGrid(StringBuilder text, int itemsNumber) {
+        final List<String> grid = new ArrayList<>();
+        while (text.length() != 0) {
+            if (itemsNumber < text.length()) {
+                grid.add(text.substring(0, itemsNumber));
+                text.delete(0, itemsNumber);
+            } else {
+                grid.add(text.toString());
+                text.delete(0, text.length());
+            }
+        }
+        return grid;
     }
 
     private String prepareInput(String message) {
--- src/test/java/javaclasses/encryptor/EncryptorTest.java
+++ src/test/java/javaclasses/encryptor/EncryptorTest.java
@@ -17,11 +17,29 @@ public class EncryptorTest {
     }
 
     @Test
-    public void testEncrypt(){
-        final String input = "have a nice day";
-        final String expected = "hae and via ecy";
+    public void testEncryptOneWord(){
+        final String input = "chillout";
+        final String expected = "clu hlt io";
         final String result = encryptor.encrypt(input);
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void testEncryptPhrase(){
+        final String input = "feed the dog";
+        final String expected = "fto ehg ee dd";
+        final String result = encryptor.encrypt(input);
+        assertEquals(expected, result);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNullInput(){
+        encryptor.encrypt(null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testEmptyString(){
+        encryptor.encrypt("");
     }
 
 }
