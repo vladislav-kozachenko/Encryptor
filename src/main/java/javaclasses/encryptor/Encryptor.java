436f65aac9f7c410210d679f6a6c27b970b609d1
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-08 12:55:46 +0200

    Created encryptor interface with public API.

---
+++ src/main/java/javaclasses/encryptor/Encryptor.java
@@ -0,0 +1,11 @@
+package javaclasses.encryptor;
+
+
+/**
+ * Represents cryptographer that receives String text and returns encrypted String output.
+ */
+public interface Encryptor {
+
+    String encrypt(String message);
+
+}
